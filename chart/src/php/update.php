a0086e81ba565512b3aacad4548955eefa5fbfad
Kitermen <105893213+Kitermen@users.noreply.github.com>
2024-06-05 23:27:19 +0200

    Delete chart directory

--- chart/src/php/update.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "charts");
-$result = mysqli_query($conn, "UPDATE chart_vals SET `value`='" . $_POST["value"] . "', `illness`='" . $_POST["illness"] . "' WHERE `day` = '" . $_POST["day"] . "'");
-
-header('Content-type: application/json');
-
-echo json_encode($_POST);
